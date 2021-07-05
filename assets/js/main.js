37515a27d4ee5d8674cc22c0c56a0324c9308571
Yitzhak A <yitzhak.alvarez1998@yahoo.com>
2021-07-05 13:30:56 -0400

    fixed a-z index search filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -197,15 +197,21 @@ function submitted(event) {
 f.addEventListener("submit", submitted);
 
 // Search Filter for A-Z index
-$(document).ready(function () {
-  $(".default_option").click(function () {
-    $(".dropdown ul").addClass("active");
-  });
-
-  $(".dropdown ul li").click(function () {
-    var text = $(this).text();
-    $(".default_option").text(text);
-    $(".dropdown ul").removeClass("active");
-  });
-});
-
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
